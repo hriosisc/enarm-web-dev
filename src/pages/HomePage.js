d017ff2acaa7bfb7aa181b7094787f25f2647f34
HRios <hrios.isc@gmail.com>
2022-04-25 15:13:07 -0700

    Updating CSS

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
 import Discount from '../components/Discount';
 import Explore from '../components/homepage/Explore';
-import Hero from '../components/homepage/Hero';
+// import Hero from '../components/homepage/Hero';
 import Resources from '../components/Resources';
 import ValueProposal from '../components/homepage/ValueProposal';
-import Witness from '../components/homepage/Witness';
+// import Witness from '../components/homepage/Witness';
 import WidthContext from '../contexts/WidthContext';
 
 const HomePage = () => {
