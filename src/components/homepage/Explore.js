cf3861a313b53e1886d065b9fc53f0a4a0fac11c
HRios <hrios.isc@gmail.com>
2022-04-27 13:19:48 -0700

    TEsting CSS

--- src/components/homepage/Explore.js
+++ src/components/homepage/Explore.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import '../../css/homepage/Explore.css';
+import React, { useState, useEffect } from 'react';
 import iconDrStar from '../../assets/imgs/monis/PicsArt_09-18-11.18.png'
 import iconDrTablet from '../../assets/imgs/monis/PicsArt_09-18-11.21.png'
 import iconDrPencil from '../../assets/imgs/monis/PicsArt_09-21-10.28.png'
+import '../../css/homepage/Explore.css';
 
-const Explore = ({width}) => {
+
+const Explore = ({size, ismobile}) => {
+
+
+    const [mobileDevice, setMobileDevice] = useState(true);
+
+    useEffect(() => {
+        const isMobileDevice = () => {
+            if(ismobile === 'true') {
+                setMobileDevice(true)
+                return;
+            }
+            setMobileDevice(false);
+            return;
+        }
+
+        isMobileDevice();
+    }, [ismobile]);
 
     return (
-        <div className='explore'>
+        <div className={`explore ${size}`}>
             <div className='explore-container'>
                 <div className='explore-container-head'>
                     <h1 className='tiny-blue-title text-center'>EXPLORA</h1>
-                    <h1 className={width !== "extra-large" ? 'subtitle white text-center' : 'bold-39 text-center white' }>Experimenta nuestra plataforma con el contenido gratuito que tenemos para ti.</h1>
+                    <h1 className={mobileDevice ? 'subtitle white text-center' : 'bold-39 text-center white' }>
+                        Experimenta nuestra plataforma con el contenido gratuito que tenemos para ti.
+                    </h1>
                 </div>
                 <div className='explore-container-body'>
                     <div className='explore-cards'>
@@ -21,7 +40,9 @@ const Explore = ({width}) => {
                             </div>
                             <div className='card-item-text'>
                                 <h1 className='bold-14 white'>Primera ventaja</h1>
-                                <p className={width !== 'extra-large' ? 'regular-14 white' : 'regular-16 white'}>Esta tarjeta debería destacar detalles de la primer ventaja de obtener el Demo.</p>                                
+                                <p className={mobileDevice? 'regular-14 white' : 'regular-16 white'}>
+                                    Esta tarjeta debería destacar detalles de la primer ventaja de obtener el Demo.
+                                </p>
                             </div>
                         </div>
                         <div className='card-item'>
@@ -30,7 +51,9 @@ const Explore = ({width}) => {
                             </div>
                             <div className='card-item-text'>
                                 <h1 className='bold-14 white'>Segunda ventaja</h1>
-                                <p className={width !== 'extra-large' ? 'regular-14 white' : 'regular-16 white'}>Esta tarjeta debería destacar detalles de la segunda ventaja de obtener el Demo.</p>
+                                <p className={mobileDevice? 'regular-14 white' : 'regular-16 white'}>
+                                    Esta tarjeta debería destacar detalles de la segunda ventaja de obtener el Demo.
+                                </p>
                             </div>
                         </div>
                         <div className='card-item'>
@@ -39,7 +62,9 @@ const Explore = ({width}) => {
                             </div>
                             <div className='card-item-text'>
                                 <h1 className='bold-14 white'>Tercera ventaja</h1>
-                                <p className={width !== 'extra-large' ? 'regular-14 white' : 'regular-16 white'}>Esta tarjeta debería destacar detalles de la tercera ventaja de obtener el Demo.</p>
+                                <p className={mobileDevice? 'regular-14 white' : 'regular-16 white'}>
+                                    Esta tarjeta debería destacar detalles de la tercera ventaja de obtener el Demo.
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -48,21 +73,21 @@ const Explore = ({width}) => {
                             <div className='form-group'>
                                 <label className='medium-14 white' htmlFor='name'>Nombre completo*</label>
                                 <input type='text' placeholder='Nombre completo' name="name" inputMode='text'/>
-                                <span className="regular-4 red">Error Message...</span>
+                                <span className="regular-14 red">Error Message...</span>
                             </div>
                             <div className='form-group'>
                                 <label className='medium-14 white' htmlFor="email">Correo electrónico*</label>
-                                <input type="text" placeholder='Correo electrónico' name="email" inputMode='email'/>
-                                <span className="regular-4 red">Error Message...</span>
+                                <input type="text" placeholder='Tu correo electrónico' name="email" inputMode='email'/>
+                                <span className="regular-14 red">Error Message...</span>
                             </div>
                             {
-                                width !== 'extra-large' ? (
+                                mobileDevice? (
                                     <>
                                         <div className='whatsapp-input'>
                                             <div className='form-group'>
-                                                <label className='medium-14 white' htmlFor="whatsapp">Whatsapp <i>(opcional)</i></label>
-                                                <input type="text" placeholder='Tu numero de Whatsapp' name='whatsapp' inputMode='text'/>
-                                                <span className="regular-4 red">Error Message...</span>
+                                                <label className='medium-14 white' htmlFor="whatsapp">Whatsapp <i className='regular-14 italic'>(opcional)</i></label>
+                                                <input type="text" placeholder='Tu teléfono' name='whatsapp' inputMode='text'/>
+                                                <span className="regular-14 red">Error Message...</span>
                                             </div>
                                         </div>
                                         <button className='button-rounded-blue-48' type='submit'>
@@ -74,11 +99,11 @@ const Explore = ({width}) => {
                                         <div className='form-group'>
                                             <label className='medium-14 white' htmlFor="whatsapp">Whatsapp <i>(opcional)</i></label>
                                             <input type="text" placeholder='Tu numero de Whatsapp' name='whatsapp' inputMode='text'/>
-                                            <span className="regular-4 red">Error Message...</span>
+                                            <span className="regular-14 red">Error Message...</span>
                                         </div>
                                         <button className='button-rounded-blue-48' type='submit'>
                                             <span className='button-text'>Quiero mi Demo</span>
-                                        </button> 
+                                        </button>
                                     </div>
                                 )
                             }
