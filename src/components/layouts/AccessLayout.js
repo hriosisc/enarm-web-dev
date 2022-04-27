a1251d80d9fd578295d635ae44c67aae79716fe6
HRios <hrios.isc@gmail.com>
2022-04-27 11:08:32 -0700

    TEsting CSS

--- src/components/layouts/AccessLayout.js
+++ src/components/layouts/AccessLayout.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import NavBar from '../NavBar'; 
 import Modal from '../Modal';
-import '../../css/Layout.css';
 import WidthContext from '../../contexts/WidthContext';
 
 const AccessLayout = ({ children }) => {
@@ -9,13 +8,13 @@ const AccessLayout = ({ children }) => {
     const width = useContext(WidthContext); 
 
     return (
-        <div className='main-container'>
+        <>
             <NavBar breakpoint={width} />
             <main className={width}>
                 { children }      
             </main>
             <Modal breakpoint={width}/>    
-        </div>
+        </>
     )
 }
 
