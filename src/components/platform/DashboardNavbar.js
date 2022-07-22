99bf4dbb10d7a840e40096609f8fb09db4533e81
HRios <hrios.isc@gmail.com>
2022-07-22 14:58:49 -0700

    Update 22/07/2022

--- src/components/platform/DashboardNavbar.js
+++ src/components/platform/DashboardNavbar.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../css/platform/components/NavbarHeader.css';
 import userIcon from '../../assets/icons/platform/default-user-icon.png'
 
 const DashboardNavbar = () => {
+
+    const [usermodal, setUserModal] = useState(false);
+ 
     return (
         <header className='platform-el'>
             <nav className='header-nav'>
@@ -12,17 +15,33 @@ const DashboardNavbar = () => {
                             <i className="material-icons-outlined">search</i>
                         </button>
                     </div>
-                    <div className="about-tag">                    
+                    <div className="about-tag">
                         <button>
                             <i className="material-icons-outlined">help_outline</i>
                         </button>
                     </div>
-                    <div className="user-info">
+                    <div className="user-info" onMouseEnter={() => setUserModal(true)}>
                         <img src={userIcon} alt="user-img" className="user-icon" />
                         <span className="user-name">Nombre del usuario</span>
                     </div>
+                    {
+                        usermodal && (
+                            <div className='u-modal-user' onMouseLeave={()=> setUserModal(false)}>
+                                <div className="u-modal-container">
+                                    <div>
+                                        <i className="material-icons">settings</i>
+                                        <span className='roboto-14'>Mi cuenta</span>
+                                    </div>
+                                    <div>
+                                        <i className="material-icons">logout</i>
+                                        <span className='roboto-14'>Cerrar Sesión</span>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
-            </nav>
+            </nav> 
         </header>
     )
 }
